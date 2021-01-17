2d6ffe753a31c722f5338cdf2dd6ee452e814ef5
abinaie <amir.b.kondolojy@gamil.com>
2021-01-17 11:46:17 -0500

    face rocognition APi clarifai

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
-import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+//import Particles from 'react-particles-js';
+
+const app = new Clarifai.App({
+  apikey: '8481393ddce6419fa6187d136d4e73dc'
+});
 
 // const particlesOptions = {
 //   particles: {
@@ -18,20 +23,47 @@ import Particles from 'react-particles-js';
 //   }
 // }
 
-function App() {
-  return (
-    <div className="App">
-     {/* <Particles classNmae='particles' params={particlesOptions}/>*/}
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-     { 
-      /*
-      <FaceRecognition/>
-     */}
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+  
+  onButtonSubmit = () =>{
+    console.log('click!')
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response){
+        console.log(response);
+      },
+      function(err){
+        //action1
+      }
+    )
+  }
+
+  render(){
+    return (
+      <div className="App">
+       {/* <Particles classNmae='particles' params={particlesOptions}/>*/}
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange = {this.onInputChange}  onSubmit = {this.onButtonSubmit}/>
+       { 
+        /*
+        <FaceRecognition/>
+       */}
+      </div>
+    );    
+  }
 }
+
+
 
 export default App;
